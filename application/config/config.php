9d9867a215930963d99606df01353631c6eb5aa6
Mubashar Iftikhar <62559139+mubashariftikhar@users.noreply.github.com>
2025-01-27 17:01:16 +0500

    initial commit

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/CodeIgniter/CRUD_APP_CI_3/';
+$config['base_url'] = 'http://localhost/CodeIgniter/CRUD_APP_Ci_3/';
 
 /*
 |--------------------------------------------------------------------------
